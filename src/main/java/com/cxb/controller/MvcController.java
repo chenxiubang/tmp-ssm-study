54905bdccaef488711be4840d0d7ee87ab689d4a
omen <1094978730@qq.com>
2021-05-23 11:52:40 +0800

    submit

--- src/main/java/com/cxb/controller/MvcController.java
+++ src/main/java/com/cxb/controller/MvcController.java
@@ -1,12 +1,22 @@
 package com.cxb.controller;
 
+import com.cxb.mapper.MvcMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.Map;
+
 @RestController//前端控制器这里好像还是要用注解+扫描，配置xml的bean好像不管用
 public class MvcController {
+    @Resource
+    private MvcMapper mvcMapper;
     @GetMapping("/test")
     public String test() {
         return "hello spring mvc";
+    }
+    @GetMapping
+    public Map test2() {
+        return  mvcMapper.method();
     }
 }
--- src/main/java/com/cxb/mapper/MvcMapper.java
+++ src/main/java/com/cxb/mapper/MvcMapper.java
@@ -1,4 +1,10 @@
 package com.cxb.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Map;
+
+@Mapper
 public interface MvcMapper {
+    Map method();
 }
